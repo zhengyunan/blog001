aa864c561c613433c1c5168776204616e0557a9d
郑宇楠 <zhengyunan@czxy.com>
2018-09-17 10:21:59 +0800

    账号切换全部完成

--- controllers/ToolController.php
+++ controllers/ToolController.php
@@ -13,6 +13,9 @@ class ToolController
     }
 
     public function login(){
+        if(config('mode')!='dev'){
+            die('非法访问');
+        }
         $email = $_GET['email'];
         // 退出
         $_SESSION=[];
